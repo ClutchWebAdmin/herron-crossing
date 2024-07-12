531edab2f6003686334cc41ce6957bac05d7319b
Gabe Wolford <gabewolford@gmail.com>
2024-07-12 14:23:07 -0700

    decreased hero font weight for mobile

--- src/app/components/Hero.jsx
+++ src/app/components/Hero.jsx
@@ -17,7 +17,7 @@ export default function Hero({ variant, media }) {
         </video>
         <div className="absolute top-0 bottom-0 left-0 right-0 w-full h-full pt-[var(--mobileHeaderHeight)] lg:pt-[var(--desktopHeaderHeight) p-5 text-white">
           <div className="flex flex-col lg:flex-row gap-5 w-full h-full pt-5">
-            <h1 className="flex justify-start items-start font-semibold text-h2 md:text-h1 lg:text-h2 xl:text-h1 w-fit">
+            <h1 className="flex justify-start items-start font-medium md:font-semibold text-h2 md:text-h1 lg:text-h2 xl:text-h1 w-fit">
               390 Acres
             </h1>
             <div className="flex-1 lg:mt-6 lg:mb-6 xl:mt-10 xl:mb-8">
@@ -26,7 +26,7 @@ export default function Hero({ variant, media }) {
                 <div className="flex w-full lg:w-1/5 h-2/3 lg:h-full border-r-2 lg:border-r-0 lg:border-l-2 lg:border-b-2 border-white"></div>
               </div>
             </div>
-            <h1 className="font-semibold text-h2 md:text-h1 lg:text-h2 xl:text-h1 w-full lg:w-fit flex justify-end items-end">
+            <h1 className="font-medium md:font-semibold text-h2 md:text-h1 lg:text-h2 xl:text-h1 w-full lg:w-fit flex justify-end items-end">
               $25,000,000
             </h1>
           </div>
