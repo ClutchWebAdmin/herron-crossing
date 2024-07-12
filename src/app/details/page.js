aff9bbcbbca83cffdd5113454b3d80566190990d
Gabe Wolford <gabewolford@gmail.com>
2024-07-12 13:49:51 -0700

    created intial detail and photos pages

---
+++ src/app/details/page.js
@@ -0,0 +1,7 @@
+export default function DetailsPage() {
+  return (
+    <main className="mt-[var(--mobileHeaderHeight)] lg:mt-[var(--desktopHeaderHeight)] px-5">
+      Details Page
+    </main>
+  );
+}
