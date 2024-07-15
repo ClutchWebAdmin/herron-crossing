c8e30ebd4595d5e86787b473f5bc9d044424be3d
Gabe Wolford <gabewolford@gmail.com>
2024-07-15 15:33:45 -0700

    fixed unescaped apostrophes, swapped image, added alt text

--- src/app/components/SummarySection.jsx
+++ src/app/components/SummarySection.jsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Button from "./UI/Button";
 import { MdArrowDownward } from "react-icons/md";
-import photo from "../../../public/images/hero-2.png";
+import photo from "../../../public/images/summary-section-view.png";
 
 export default function SummarySection() {
   return (
@@ -14,25 +14,26 @@ export default function SummarySection() {
           <h3 className="text-h3 lg:text-h2 font-medium">Executive summary</h3>
           <div className="flex flex-col gap-5 mb-10 text-h6 lg:text-h5">
             <p>
-              Prime development opportunity inside the City of Salem limits.
-              Situated just 7 miles southeast of downtown Salem, this property
-              is the last and largest of its kind within city boundaries. It
-              shows off with a unique blend of rural tranquility, impressive
-              natural features, and suburban convenience.
+              Herron Crossing is a prime development opportunity inside the City
+              of Salem limits. Situated just 7 miles southeast of downtown
+              Salem, this property is the last and largest of its kind within
+              city boundaries. It shows off with a unique blend of rural
+              tranquility, impressive natural features, and suburban
+              convenience.
             </p>
             <p>
               The property neighbors Turner Lake and Corban University with easy
               access to major amenities including an airport and big-box retail.
-              One of the best features of the site is one that can't be seen -
-              the residential areas at the southern end of the property are
-              within the Cascade School District, a highly sought after district
-              within the Willamette Valley.
+              One of the best features of the site is one that can&apos;t be
+              seen - the residential areas at the southern end of the property
+              are within the Cascade School District, a highly sought after
+              district within the Willamette Valley.
             </p>
             <p>
               Seize the chance to shape the future of this evolving
-              rural-suburban area. Whether you're looking to invest in a premier
-              development project or seeking a strategic partnership, this
-              property promises unparalleled potential.
+              rural-suburban area. Whether you&apos;re looking to invest in a
+              premier development project or seeking a strategic partnership,
+              this property promises unparalleled potential.
             </p>
           </div>
           <Button
@@ -48,9 +49,10 @@ export default function SummarySection() {
       <div className="w-full h-full sticky top-0 right-0 lg:w-1/2">
         <Image
           src={photo}
-          alt="placeholder"
+          alt="Turner road field in the morning sun"
           className="w-full h-auto object-cover aspect-square"
           placeholder="blur"
+          quality={100}
         />
       </div>
     </section>
