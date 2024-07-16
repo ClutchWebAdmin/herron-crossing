e47eb81cd8c1cce6504be782d25b4651a863d9dc
Gabe Wolford <gabewolford@gmail.com>
2024-07-16 15:46:54 -0700

    added highlights to summary paragraph

--- src/app/details/page.js
+++ src/app/details/page.js
@@ -9,6 +9,7 @@ export default function DetailsPage() {
   const paragraphs = [
     "Herron Crossing is a prime development opportunity inside the City of Salem limits. Situated just 7 miles southeast of downtown Salem, 1.1 miles from Interstate 5, and 1.9 miles from Highway 22, this property is the last and largest of its kind within city boundaries. It shows off with a unique blend of rural tranquility, impressive natural features, and suburban convenience.",
     "The property neighbors Turner Lake and Corban University with easy access to major amenities including an airport and big-box retail. One of the best features of the site is one that can't be seen - the residential areas at the southern end of the property are within the Cascade School District, a highly sought after district within the Willamette Valley.",
+    "Some highlights include already in-place income of over $18k/mo, joint venture partnership opportunities, flexible options, zoning change complete, nature trails and recreation possibilities with a proposed 80-acre park with over a mile of Mill Creek Access.",
     "Seize the chance to shape the future of this evolving rural-suburban area. Whether you're looking to invest in a premier development project or seeking a strategic partnership, this property promises unparalleled potential.",
   ];
   return (
