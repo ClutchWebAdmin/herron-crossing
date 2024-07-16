13909cdab3d2eb133c5214548de8a82bd562264f
Gabe Wolford <gabewolford@gmail.com>
2024-07-16 09:56:15 -0700

    added props to summary section component to encourage reuse

--- src/app/components/SummarySection.jsx
+++ src/app/components/SummarySection.jsx
@@ -1,9 +1,16 @@
 import Image from "next/image";
 import Button from "./UI/Button";
-import { MdArrowDownward } from "react-icons/md";
-import photo from "../../../public/images/summary-section-image.png";
 
-export default function SummarySection() {
+export default function SummarySection({
+  heading,
+  paragraphs,
+  buttonVariant,
+  linkTo,
+  buttonText,
+  buttonIcon,
+  image,
+  altText,
+}) {
   return (
     <section
       id="summary"
@@ -11,45 +18,25 @@ export default function SummarySection() {
     >
       <div className="flex flex-col gap-10 w-full lg:w-1/2">
         <div className="flex flex-col h-full justify-between gap-20">
-          <h3 className="text-h3 lg:text-h2 font-medium">Executive summary</h3>
+          <h3 className="text-h2 font-medium">{heading}</h3>
           <div className="flex flex-col gap-5 mb-10 text-h6 lg:text-h5">
-            <p>
-              Herron Crossing is a prime development opportunity inside the City
-              of Salem limits. Situated just 7 miles southeast of downtown
-              Salem, this property is the last and largest of its kind within
-              city boundaries. It shows off with a unique blend of rural
-              tranquility, impressive natural features, and suburban
-              convenience.
-            </p>
-            <p>
-              The property neighbors Turner Lake and Corban University with easy
-              access to major amenities including an airport and big-box retail.
-              One of the best features of the site is one that can&apos;t be
-              seen - the residential areas at the southern end of the property
-              are within the Cascade School District, a highly sought after
-              district within the Willamette Valley.
-            </p>
-            <p>
-              Seize the chance to shape the future of this evolving
-              rural-suburban area. Whether you&apos;re looking to invest in a
-              premier development project or seeking a strategic partnership,
-              this property promises unparalleled potential.
-            </p>
+            {paragraphs.map((item, index) => (
+              <p key={index}>{item}</p>
+            ))}
           </div>
           <Button
             isLight
-            variant={`double`}
-            linkTo={"#details"}
-            text={`View all details`}
-            icon={<MdArrowDownward />}
+            variant={buttonVariant}
+            linkTo={linkTo}
+            text={buttonText}
+            icon={buttonIcon}
           />
         </div>
       </div>
-
       <div className="w-full h-full sticky top-0 right-0 lg:w-1/2">
         <Image
-          src={photo}
-          alt="Turner road"
+          src={image}
+          alt={altText}
           className="w-full h-auto object-cover aspect-square saturate-150"
           placeholder="blur"
         />
--- src/app/details/page.js
+++ src/app/details/page.js
@@ -1,9 +1,16 @@
 import HeroPhoto from "../components/HeroPhoto";
 import hero from "../../../public/images/hero.png";
-import { MdArrowOutward } from "react-icons/md";
+import { MdArrowDownward, MdArrowOutward } from "react-icons/md";
 import SummarySection from "../components/SummarySection";
+import DetailsSection from "../components/DetailsSection";
+import summaryImage from "../../../public/images/summary-section-image.png";
 
 export default function DetailsPage() {
+  const paragraphs = [
+    "Herron Crossing is a prime development opportunity inside the City of Salem limits. Situated just 7 miles southeast of downtown Salem, 1.1 miles from Interstate 5, and 1.9 miles from Highway 22, this property is the last and largest of its kind within city boundaries. It shows off with a unique blend of rural tranquility, impressive natural features, and suburban convenience.",
+    "The property neighbors Turner Lake and Corban University with easy access to major amenities including an airport and big-box retail. One of the best features of the site is one that can't be seen - the residential areas at the southern end of the property are within the Cascade School District, a highly sought after district within the Willamette Valley.",
+    "Seize the chance to shape the future of this evolving rural-suburban area. Whether you're looking to invest in a premier development project or seeking a strategic partnership, this property promises unparalleled potential.",
+  ];
   return (
     <main>
       <HeroPhoto
@@ -16,7 +23,17 @@ export default function DetailsPage() {
         linkTo={`#summary`}
         icon={<MdArrowOutward />}
       />
-      <SummarySection />
+      <SummarySection
+        heading={`Summary`}
+        paragraphs={paragraphs}
+        buttonVariant={`double`}
+        linkTo={`#details`}
+        buttonText={`View all details`}
+        buttonIcon={<MdArrowDownward />}
+        image={summaryImage}
+        altText={`Chain hanging on a steel beam`}
+      />
+      {/* <DetailsSection /> */}
     </main>
   );
 }
