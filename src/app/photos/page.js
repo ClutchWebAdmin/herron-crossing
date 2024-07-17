86610073822d7b92fa06faa1cad8bde15c2d7680
Gabe Wolford <gabewolford@gmail.com>
2024-07-17 12:47:44 -0700

    added carousels to photo page

--- src/app/photos/page.js
+++ src/app/photos/page.js
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import photo1 from "../../../public/images/prison-exterior-1.png";
 import photo2 from "../../../public/images/prison-exterior-2.png";
 import photo3 from "../../../public/images/prison-exterior-3.png";
@@ -11,6 +10,7 @@ import photo9 from "../../../public/images/concept-map-2.png";
 import photo10 from "../../../public/images/concept-map-3.png";
 import photo11 from "../../../public/images/concept-map-4.png";
 import photo12 from "../../../public/images/concept-map-5.png";
+import Carousel from "../components/Carousel";
 
 const propertyPhotos = [photo1, photo2, photo3, photo4, photo5, photo6, photo7];
 
@@ -20,39 +20,21 @@ export default function PhotosPage() {
   return (
     <main className="mt-[var(--mobileHeaderHeight)] lg:mt-[var(--desktopHeaderHeight)]">
       <section className="flex flex-col gap-10 px-5 pb-10 h-full w-full 2xl:w-4/6 mx-auto">
-        <div
-          id="photo-gallery"
-          className="flex flex-col gap-10 col-span-full py-10"
-        >
-          <h3 className="text-h2 font-medium col-span-full pb-5 border-b-2 border-black">
-            Photo Gallery
+        <div id="photo-gallery" className="flex flex-col gap-10 py-10">
+          <h3 className="text-h2 font-medium pb-5 border-b-2 border-black">
+            Photos
           </h3>
-          {propertyPhotos.map((photo, index) => (
-            <Image
-              key={index}
-              src={photo}
-              alt={`Photo ${index + 1}`}
-              className="w-full h-auto object-cover"
-              placeholder="blur"
-            />
-          ))}
+          <div className="col-span-full">
+            <Carousel photos={propertyPhotos} />
+          </div>
         </div>
-        <div
-          id="concept-maps"
-          className="flex flex-col gap-10 col-span-full py-10"
-        >
-          <h3 className="text-h2 font-medium col-span-full pb-5 border-b-2 border-black">
+        <div id="concept-maps" className="flex flex-col gap-10  py-10">
+          <h3 className="text-h2 font-medium pb-5 border-b-2 border-black">
             Concept Maps
           </h3>
-          {conceptMaps.map((photo, index) => (
-            <Image
-              key={index}
-              src={photo}
-              alt={`Concept ${index + 1}`}
-              className="w-full h-auto object-cover"
-              placeholder="blur"
-            />
-          ))}
+          <div className="col-span-full">
+            <Carousel photos={conceptMaps} />
+          </div>
         </div>
       </section>
     </main>
